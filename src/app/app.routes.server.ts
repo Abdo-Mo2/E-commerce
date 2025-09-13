59694db64af2a691887d23fe0ec51d91949b46e8
Abdo-Mo2 <apdalahali6@gmail.com>
2025-09-13 19:14:03 +0300

    init commit

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,7 +3,8 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   { path: 'products/:id', renderMode: RenderMode.Client },
   { path: 'categories/:id', renderMode: RenderMode.Client },
-  { path: 'brands/product/:id', renderMode: RenderMode.Client },
+  { path: 'brands/product/:id', renderMode: RenderMode.Client },
+  { path: 'brands/:id/:name', renderMode: RenderMode.Client },
   {
     path: '**',
     
